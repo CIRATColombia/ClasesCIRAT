94e1671c0c9b36dd86ee6111727172d4be434b9c
Gaberdugo <gaberdugov@unal.edu.co>
2024-06-11 15:39:06 -0500

    Nuevo Ejercicio

--- Prueba.py
+++ Prueba.py
@@ -1,8 +1,10 @@
 import random
-
+# Muy bien, programaciíon excelente
 x = random.randint(9,30)
 y = random.randint(10,50)
 
 z = x+y
 
 print(z)
+
+# Nuevo ejercicio, un programa que genere un número aleatorio y lo clasifique como primo o no primo 
